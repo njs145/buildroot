8b0b2d32abac4d462ac4a351b6d5efe09f5fd020
jinsong <njs145@naver.com>
2025-07-12 16:08:00 +0900

    build 스크립트 수정

--- qemu_build.sh
+++ qemu_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#make qemu_aarch64_virt_defconfig
-#utils/config -e BR2_TARGET_ROOTFS_CPIO
-#utils/config -e BR2_TARGET_ROOTFS_CPIO_GZIP
+make qemu_aarch64_virt_defconfig
+utils/config -e BR2_TARGET_ROOTFS_CPIO
+utils/config -e BR2_TARGET_ROOTFS_CPIO_GZIP
 make olddefconfig
 echo "The build is running in the background. To check on it, run: tmux attach -t build_root-build"
 echo "To detach and return to your shell, press Ctrl + b, then d."
